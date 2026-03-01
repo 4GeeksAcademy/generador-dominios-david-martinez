c4ce867d5d72b69c803d276b6c892c2f2246b2d6
DLMARTINEZ <122575833+Estimatus@users.noreply.github.com>
2026-03-01 15:06:09 +0000

    correcciones pedidas por erwing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.us', '.io'];
+
+  let domainListElement = document.querySelector("#domain-list");
+  let allDomainsHTML = ""; // Variable para acumular el código HTML
+
+  // Bucles anidados 
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extensions) {
+          let domainName = p + a + n + e;
+          console.log(domainName); // Sigue saliendo en consola
+          
+          // Creo un elemento de lista para cada dominio
+          allDomainsHTML += `<li class="list-group-item">${domainName}</li>`;
+        }
+      }
+    }
+  }
+
+  // Inyecto todo el HTML acumulado en el <ul>
+  domainListElement.innerHTML = allDomainsHTML;
 };
